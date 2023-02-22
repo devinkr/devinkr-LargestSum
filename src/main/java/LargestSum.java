d21f8bb9089a8aeba93122b5c4336c415558a3c0
Devin Raleigh <devinkr@gmail.com>
2023-02-22 22:28:23 +0000

    Largest Sum

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -10,6 +10,15 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        int largest = Integer.MIN_VALUE;
+        for (int i =0; i < nums.size(); i++) {
+            for (int j = i +1; j < nums.size(); j++) {
+                int total = nums.get(i) + nums.get(j);
+                if (total > largest) {
+                    largest = total;
+                }
+            }
+        }
+        return largest;
     }
 }
